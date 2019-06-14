f191a1dc5ca080f037180cc630e9d3239849c492
xXxDarkGodxXx <32574257+xXxDarkGodxXx@users.noreply.github.com>
2019-06-14 20:57:55 +0300

    Add files via upload

---
+++ omni_TP706A.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/Neffos/TP706A/TP706A.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := TP706A
+PRODUCT_NAME := omni_TP706A
+PRODUCT_BRAND := Neffos
+PRODUCT_MODEL := Neffos_C9A
+PRODUCT_MANUFACTURER := TP-Link
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo omni_TP706A-userdebug
